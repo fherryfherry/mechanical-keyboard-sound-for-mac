514012b3c36eda4ca9811c82115d1cdc7f8348e7
Ferry <ferdevelop15@gmail.com>
2026-04-09 15:26:38 +0700

    Initial Commit

---
+++ Mechanical Sound For Mac/Mechanical_Sound_For_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mechanical_Sound_For_MacApp.swift
+//  Mechanical Sound For Mac
+//
+//  Created by Ferry on 09/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Mechanical_Sound_For_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
